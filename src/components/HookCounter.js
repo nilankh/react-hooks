a2d4fea86f7f4def366082f2ed880d024e0fc6e5
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-15 22:46:38 +0530

    useStatelecture2 done

---
+++ src/components/HookCounter.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+// first step is to create a component
+function HookCounter() {
+    // second step is to create a state variable initialized to 0
+    // the syntax here is known as array destructuring 
+    // the last step is to create a method that is(setCount) capable of setting this state value
+    const [count, setCount ] = useState(0)
+    // this hook or function accepts an arguments which is the initial value of the state property and returns the current value of the state property and method that is capable of updating that state property
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Count {count}</button>
+        </div>
+    )
+    
+}
+
+export default HookCounter
