3effb4e26cac2c6217772c1835c901911525ea8d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-15 22:28:19 +0530

    learning about useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   return (
     <div className="App">
-      
+      <ClassCounter />
     </div>
   );
 }
--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -1,20 +1,25 @@
 // hooks lecture2
 
 import React, { Component } from 'react'
-
+// first step is to create a component and we have  to used a class component
 class ClassCounter extends Component {
     constructor(props) {
         super(props)
-    
+    // second step is to create a state variable initialized to 0
         this.state = {
              count: 0
         }
     }
-    
+    // the last step is to create a method that is capable of setting this state value
+    incrementCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
     render() {
         return (
             <div>
-                
+                <button onClick={this.incrementCount}>Count {this.state.count}</button>
             </div>
         )
     }
