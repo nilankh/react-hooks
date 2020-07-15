882fed4dd08466019d11b613fda8e6e55ca50c21
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-15 22:23:51 +0530

    started learning about useState hook

---
+++ src/components/ClassCounter.js
@@ -0,0 +1,23 @@
+// hooks lecture2
+
+import React, { Component } from 'react'
+
+class ClassCounter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             count: 0
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ClassCounter
