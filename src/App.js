57cd596ad3d21e15a1fe7494e9972e4cb6ba8954
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-05 17:55:05 +0530

    done with useState with array

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 // import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
 import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter3 />
+      <HookCounter4 />
+      {/* <HookCounter3 /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
