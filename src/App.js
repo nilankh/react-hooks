84a1d9f52cc9e4e9079ceeb77cc781a8b3ddd472
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-07 07:59:06 +0530

    fetching data with useEffect part1

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ import HookMouse from "./components/HookMouse";
 import MouseContainer from "./components/MouseContainer";
 import IntervalClassCounter from "./components/IntervalClassCounter";
 import IntervalHookCounter from "./components/IntervalHookCounter";
+import DataFetching from "./components/DataFetching";
 
 function App() {
   return (
     <div className="App">
-      <IntervalHookCounter />
-      <IntervalClassCounter />
+
+      <DataFetching />
+      {/* <IntervalHookCounter />
+      <IntervalClassCounter /> */}
       {/* <MouseContainer /> */}
       {/* <HookMouse /> */}
       {/* <ClassMouse /> */}
